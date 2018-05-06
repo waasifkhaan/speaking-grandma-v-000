bc8badd58f22c55931904827dfba8aa3d6fd1b5a
Wasif Khan <waasif.khaan@gmail.com>
2018-05-06 19:53:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
+  elsif string == ""
   elsif string.upcase == "string"
     "NO, NOT SINCE 1938!"
   elsif  string.downcase == string  
