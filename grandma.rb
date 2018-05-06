c96752a583bbb42904299dee86b653438319ef67
Wasif Khan <waasif.khaan@gmail.com>
2018-05-06 18:36:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string.uppercase == string 
-    puts ""
+  if string.uppercase !== string 
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif string.uppercase !== string
+  puts ""
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
