bb3fc7bcde1da57f97eef56fd06ea61a17c5bc92
Wasif Khan <waasif.khaan@gmail.com>
2018-05-06 20:45:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,10 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif string == "Hi Nana, how are you?"
-    "HUH?! SPEAK UP, SONNY!"
   elsif  string.upcase == string  
     "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
