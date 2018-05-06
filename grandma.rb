cbf4e9057227064c50bc875e9d08bc9c4f24ff9c
Wasif Khan <waasif.khaan@gmail.com>
2018-05-06 19:48:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,8 +6,7 @@ def speak_to_grandma(string)
     "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase == "string"
     "NO, NOT SINCE 1938!"
-  elsif 
-end
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
