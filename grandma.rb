0e49075ba9b6430f34128c64e0998351451eab75
Wasif Khan <waasif.khaan@gmail.com>
2018-05-06 19:54:47 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,9 +5,10 @@ def speak_to_grandma(string)
   elsif string == "Hi Nana, how are you?"
     "HUH?! SPEAK UP, SONNY!"
   elsif string == "Hi"
-    "NO, NOT SINCE 1938!"
-  elsif  string.downcase == string  
     "HUH?! SPEAK UP, SONNY!"
+  elsif  string.downcase == string  
+    "NO, NOT SINCE 1938!"
+    
   
   end
 end
