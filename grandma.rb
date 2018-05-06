6d61cdc135dff2aaa9d2189db8f448cdade82863
Wasif Khan <waasif.khaan@gmail.com>
2018-05-06 18:37:35 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@ def speak_to_grandma(string)
   if string.uppercase !== string 
     puts "HUH?! SPEAK UP, SONNY!"
   elsif string.uppercase !== string
-  puts ""
+  puts "NO, NOT SINCE 1938!"
+else if string ==""
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
