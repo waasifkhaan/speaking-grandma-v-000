436a70bd6eacb130dc07fe0364d8ec274f72cb10
Wasif Khan <waasif.khaan@gmail.com>
2018-05-06 18:45:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  #if string.upcase !== string 
-    #puts "HUH?! SPEAK UP, SONNY!"
+  if string.upcase !== string 
+    "HUH?! SPEAK UP, SONNY!"
   if string.upcase == "string"
     "NO, NOT SINCE 1938!"
   elsif string =="I LOVE YOU GRANDMA!"
